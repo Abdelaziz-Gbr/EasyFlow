c95cb572a4a001b7459b418d6bed70676a11f664
Abdelaziz-Gbr <84877625+Abdelaziz-Gbr@users.noreply.github.com>
2023-07-03 18:58:19 +0300

    -fixed a pug in retrieving null end time in history.

--- app/src/main/java/com/easyflow/network/models/TicketNetworkModel.kt
+++ app/src/main/java/com/easyflow/network/models/TicketNetworkModel.kt
@@ -5,12 +5,12 @@ import com.easyflow.database.models.TicketDatabaseModel
 data class TicketNetworkModel (
     val id: String,
     val startTime: String,
-    val endTime: String,
+    val endTime: String?,
     val status: String,
     val startStation: String,
     val price: Float,
     val endStation: String)
 
 fun TicketNetworkModel.toDatabaseDomain(): TicketDatabaseModel{
-    return TicketDatabaseModel(id, startTime, endTime, status, startStation, price, endStation)
+    return TicketDatabaseModel(id, startTime, endTime?: "null", status, startStation, price, endStation)
 }
