5fdacf9515f8583184caeeeee93f9f9c7b987ff5
Abdelaziz-Gbr <84877625+Abdelaziz-Gbr@users.noreply.github.com>
2023-07-06 11:22:36 +0300

    final bug fix server response not showing in plan subscription.

--- app/src/main/java/com/easyflow/appScreens/services/fragmentPlans/planDetails/PlanDetailsFragment.kt
+++ app/src/main/java/com/easyflow/appScreens/services/fragmentPlans/planDetails/PlanDetailsFragment.kt
@@ -35,7 +35,6 @@ class PlanDetailsFragment : Fragment() {
                     show()
                 }
             }
-            viewModel.onResponseRecieved()
 
         }
         return binding.root
