a41d1c64f70eed0256847ea4e4e2200f9ea0cb5d
Abdelaziz-Gbr <84877625+Abdelaziz-Gbr@users.noreply.github.com>
2023-07-08 20:37:11 +0300

    setting the serverIP address - final commit isa

--- app/src/main/java/com/easyflow/utils/Constants.kt
+++ app/src/main/java/com/easyflow/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.easyflow.utils
 
 object Constants {
-    const val serverBaseURL = "http://192.168.1.110:8080/"
+    const val serverBaseURL = "http://ec2-13-48-43-236.eu-north-1.compute.amazonaws.com/"
     const val channelID = "easyFlow_notificationChannel"
     const val channelName = "com.easyflow.firebaseMessagingService"
 
